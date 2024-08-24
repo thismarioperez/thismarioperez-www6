69b1100f88242300879a65049ef56e1873237518
Mario Perez <mario.perez415@gmail.com>
2024-08-24 14:31:52 -0400

    Add glsl loaders

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,40 @@
 const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ["gsap"],
+    webpack(config, { isServer }) {
+        if (!isServer) {
+            // We're in the browser build, so we can safely exclude the sharp module
+            config.externals.push("sharp");
+        }
+
+        // audio support
+        config.module.rules.push({
+            test: /\.(ogg|mp3|wav|mpe?g)$/i,
+            exclude: config.exclude,
+            use: [
+                {
+                    loader: "url-loader",
+                    options: {
+                        limit: config.inlineImageLimit,
+                        fallback: "file-loader",
+                        publicPath: `${config.assetPrefix}/_next/static/images/`,
+                        outputPath: `${isServer ? "../" : ""}static/images/`,
+                        name: "[name]-[hash].[ext]",
+                        esModule: config.esModule || false,
+                    },
+                },
+            ],
+        });
+
+        // shader support
+        config.module.rules.push({
+            test: /\.(glsl|vs|fs|vert|frag)$/,
+            exclude: /node_modules/,
+            use: ["raw-loader", "glslify-loader"],
+        });
+
+        return config;
+    },
 };
 
 export default nextConfig;
