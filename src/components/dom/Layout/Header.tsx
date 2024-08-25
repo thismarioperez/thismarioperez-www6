faba756a2c2af6a1315df4b07a68c3ba2484d150
Mario Perez <mario.perez415@gmail.com>
2024-08-25 02:47:34 -0400

    Make header click-able

--- src/components/dom/Layout/Header.tsx
+++ src/components/dom/Layout/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header>
+        <header className="pointer-events-auto">
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
             <Link href="/projects/robin-knows">Projects - Robin Knows</Link>
--- src/components/dom/Layout/index.tsx
+++ src/components/dom/Layout/index.tsx
@@ -1,27 +1,33 @@
 "use client";
 
 import { AppProps } from "next/app";
-import MainScene from "@/components/canvas/MainScene";
+import dynamic from "next/dynamic";
 import { TransitionProvider } from "@/context/TransitionContext";
-import Header from "@/components/dom/Layout/Header";
 import TransitionComponent from "@/components/dom/Layout/TransitionComponent";
+import Header from "@/components/dom/Layout/Header";
 import DebugEvents from "@/components/dom/Layout/DebugEvents";
 import LevaUI from "@/components/dom/Layout/LevaUI";
 
+const MainScene = dynamic(() => import("@/components/canvas/MainScene"), {
+    ssr: false,
+});
+
 export default function Layout({ Component, pageProps }: AppProps) {
     return (
-        <TransitionProvider>
-            <div className="absolute top-0 left-0 size-full">
-                <MainScene />
-            </div>
-            <div className="relative size-full pointer-events-none">
-                <Header />
-                <TransitionComponent>
-                    <Component {...pageProps} />
-                </TransitionComponent>
-            </div>
-            <LevaUI />
-            <DebugEvents />
-        </TransitionProvider>
+        <>
+            <TransitionProvider>
+                <div className="absolute top-0 left-0 size-full">
+                    <MainScene />
+                </div>
+                <div className="relative size-full pointer-events-none">
+                    <Header />
+                    <TransitionComponent>
+                        <Component {...pageProps} />
+                    </TransitionComponent>
+                </div>
+                <LevaUI />
+                <DebugEvents />
+            </TransitionProvider>
+        </>
     );
 }
