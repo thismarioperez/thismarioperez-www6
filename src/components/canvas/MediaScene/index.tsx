7de0b319ce9da23143a396cc32b7dfc3043110b2
Mario Perez <mario.perez415@gmail.com>
2024-08-25 21:36:45 -0400

    Cleanup media scene

--- src/components/canvas/MediaScene/index.tsx
+++ src/components/canvas/MediaScene/index.tsx
@@ -1,16 +1,10 @@
-import useDebug from "@/hooks/useDebug";
-import { CameraControls } from "@react-three/drei";
 import { Suspense } from "react";
 import MediaMesh from "./MediaMesh";
 
 export default function MediaScene() {
-    const [debug] = useDebug();
-
     return (
         <>
-            <CameraControls enabled={debug} />
-
-            <Suspense>
+            <Suspense fallback={null}>
                 <MediaMesh src="/images/image-1.jpg" />
             </Suspense>
         </>
