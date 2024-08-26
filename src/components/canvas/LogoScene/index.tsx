534df461724e29798c4dc2e6022950253cf1496e
Mario Perez <mario.perez415@gmail.com>
2024-08-25 23:00:29 -0400

    Setup logo scene

--- src/components/canvas/LogoScene/index.tsx
+++ src/components/canvas/LogoScene/index.tsx
@@ -1,33 +1,37 @@
 import { Suspense, useRef } from "react";
 import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
-
-const RotatingCube = () => {
-    const meshRef = useRef<THREE.Mesh>(null);
-
-    useFrame(() => {
-        if (meshRef.current) {
-            meshRef.current.rotation.x += 0.01;
-            meshRef.current.rotation.y += 0.01;
-        }
-    });
-
-    return (
-        <mesh ref={meshRef} position={[0, 0, 0]}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshBasicMaterial color="magenta" />
-        </mesh>
-    );
-};
+import {
+    Bounds,
+    Center,
+    Float,
+    PerspectiveCamera,
+    PresentationControls,
+    Text3D,
+} from "@react-three/drei";
 
 export default function LogoScene() {
     return (
         <>
             <Suspense fallback={null}>
+                <PerspectiveCamera makeDefault position={[0, 0, 5]} fov={75} />
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
                 <color attach="background" args={["black"]} />
-                <RotatingCube />
+
+                <Float floatingRange={[undefined, 0.01]} speed={2}>
+                    <Center>
+                        <Text3D
+                            font="/fonts/PrestigeEliteStd_Bold.json"
+                            castShadow
+                            receiveShadow
+                            scale={5}
+                        >
+                            M
+                            <meshPhongMaterial color={"white"} />
+                        </Text3D>
+                    </Center>
+                </Float>
             </Suspense>
         </>
     );
