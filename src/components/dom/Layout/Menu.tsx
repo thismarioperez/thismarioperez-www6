ff624731db1af61b8481f9fe1e059004466b9c16
Mario Perez <mario.perez415@gmail.com>
2024-08-30 03:23:24 -0400

    Setup menu in/out animation

--- src/components/dom/Layout/Menu.tsx
+++ src/components/dom/Layout/Menu.tsx
@@ -1,7 +1,11 @@
 import cx from "classnames";
 import Link from "next/link";
 
+import { gsap, useGSAP } from "@/lib/gsap";
 import * as Themed from "@/components/dom/common/Themed";
+import { useEffect, useRef } from "react";
+import { Transition } from "react-transition-group";
+import useMenuState from "@/hooks/useMenuState";
 
 type TBaseLink = {
     name: string;
@@ -46,7 +50,7 @@ const LinkRenderer = ({
     return (
         <ul
             className={cx(
-                "pointer-events-auto flex flex-col items-start gap-y-4 text-black",
+                "pointer-events-auto flex flex-col items-start gap-y-4 text-white",
                 className
             )}
         >
@@ -54,7 +58,7 @@ const LinkRenderer = ({
                 return (
                     <li key={idx}>
                         {link.type === "link" ? (
-                            <Link href={link.href}>
+                            <Link className="js-item" href={link.href}>
                                 <Themed.ButtonText>
                                     {link.name}
                                 </Themed.ButtonText>
@@ -62,10 +66,12 @@ const LinkRenderer = ({
                         ) : null}
                         {link.type === "folder" ? (
                             <div className="flex flex-col gap-y-2">
-                                <Themed.ButtonText>
-                                    {link.name}
-                                </Themed.ButtonText>
-                                <hr className="w-full border-black" />
+                                <div className="js-item">
+                                    <Themed.ButtonText>
+                                        {link.name}
+                                    </Themed.ButtonText>
+                                    <hr className="w-full border-white" />
+                                </div>
                                 <LinkRenderer
                                     className="pl-4"
                                     links={link.children}
@@ -80,12 +86,56 @@ const LinkRenderer = ({
 };
 
 export default function Menu() {
+    const { menuOpen } = useMenuState();
+    const node = useRef(null);
+    const tl = useRef<gsap.core.Timeline>(gsap.timeline({ paused: true }));
+
+    useGSAP(
+        () => {
+            gsap.set(".js-nav", {
+                visibility: "hidden",
+            });
+            gsap.set(".js-item", {
+                opacity: 0,
+                y: -10,
+                // x: 10,
+            });
+            tl.current = gsap
+                .timeline({ paused: true, duration: 1 })
+                .to(".js-nav", {
+                    visibility: "visible",
+                    duration: 0.01,
+                })
+                .to(".js-item", {
+                    opacity: 1,
+                    y: 0,
+                    // x: 0,
+                    ease: "power1.inOut",
+                    stagger: 0.1,
+                })
+                .play();
+        },
+        {
+            scope: node,
+        }
+    );
+
+    useEffect(() => {
+        if (menuOpen) {
+            tl.current.play();
+        } else {
+            tl.current.reverse();
+        }
+    }, [menuOpen]);
+
     return (
-        <div className="fixed size-full top-0 left-0">
+        <div className="fixed size-full top-0 left-0" ref={node}>
             <div className="relative size-full p-16">
-                <nav className="flex flex-col items-end">
-                    <LinkRenderer links={LINKS} />
-                </nav>
+                <div className="flex flex-col items-end">
+                    <nav className="js-nav w-fit">
+                        <LinkRenderer links={LINKS} />
+                    </nav>
+                </div>
             </div>
         </div>
     );
