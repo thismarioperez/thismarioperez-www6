8fdaf2d20a1aae32aa42eb003caf13f8913830c8
Mario Perez <mario.perez415@gmail.com>
2024-08-30 02:02:34 -0400

    Add temp height div to test scrolling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function HomePage() {
     return (
         <div className="size-full relative">
             <h1>Home page</h1>
+            <div className="min-h-[100vh]"></div>
         </div>
     );
 }
