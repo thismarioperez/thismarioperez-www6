cd30f292d1da27b1b743076220ee8cae874b6a3a
Mario Perez <mario.perez415@gmail.com>
2024-08-30 02:02:42 -0400

    Make header sticky

--- src/components/dom/Layout/Header.tsx
+++ src/components/dom/Layout/Header.tsx
@@ -5,15 +5,19 @@ import { ReactComponent as LogoMark } from "@/svg/logo-mark.svg";
 
 export default function Header() {
     return (
-        <header className="pointer-events-auto fixed top-0 left-0">
-            <Link href="/">
-                <div className="w-72">
-                    <Logo className="fill-white" />
-                </div>
-                <div className="w-8">
-                    <LogoMark className="fill-white" />
-                </div>
-            </Link>
+        <header className="pointer-events-auto w-full sticky top-0 left-0">
+            <ul className="relative w-full flex-flex-row justify-between">
+                <li>
+                    <Link href="/">
+                        <div className="w-72">
+                            <Logo className="fill-white" />
+                        </div>
+                        <div className="w-8">
+                            <LogoMark className="fill-white" />
+                        </div>
+                    </Link>
+                </li>
+            </ul>
         </header>
     );
 }
