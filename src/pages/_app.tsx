dc210610d9f42ed00f2e81c4a2460233a3cb0960
Mario Perez <mario.perez415@gmail.com>
2024-08-30 01:49:52 -0400

    Add base meta info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { Source_Code_Pro } from "next/font/google";
 
 import Layout from "@/components/dom/Layout";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const sourceCodePro = Source_Code_Pro({
     subsets: ["latin"],
@@ -16,6 +17,18 @@ export default function App(props: AppProps) {
                     --source-code-pro-font: ${sourceCodePro.style.fontFamily};
                 }
             `}</style>
+            <Head>
+                <link
+                    rel="icon"
+                    type="image/svg+xml"
+                    href="/favicon/favicon.svg"
+                />
+                <link rel="icon" type="image/png" href="/favicon/favicon.png" />
+                <title>
+                    thismarioperez - 🤘- Personal website of creative developer
+                    Mario Perez
+                </title>
+            </Head>
             <Layout {...props} />
         </>
     );
