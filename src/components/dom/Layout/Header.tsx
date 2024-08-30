3b9bbb1df1ba0319c2d3552919d0fb663f1364a7
Mario Perez <mario.perez415@gmail.com>
2024-08-30 01:49:01 -0400

    Add logo svgs

--- src/components/dom/Layout/Header.tsx
+++ src/components/dom/Layout/Header.tsx
@@ -1,11 +1,22 @@
 import Link from "next/link";
 
+import { ReactComponent as Logo } from "@/svg/logo.svg";
+import { ReactComponent as LogoMark } from "@/svg/logo-mark.svg";
+
 export default function Header() {
     return (
-        <header className="pointer-events-auto">
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/projects/robin-knows">Projects - Robin Knows</Link>
+        <header className="pointer-events-auto fixed top-0 left-0">
+            <Link href="/">
+                <div
+                    className="w-72
+                "
+                >
+                    <Logo className="fill-white" />
+                </div>
+                <div className="w-8">
+                    <LogoMark className="fill-white" />
+                </div>
+            </Link>
         </header>
     );
 }
