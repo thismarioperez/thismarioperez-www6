7fdbb92ad814602edd32031bc7a5b83206095e3b
Mario Perez <mario.perez415@gmail.com>
2024-09-01 03:43:26 -0400

    Observe and track header height. Tweak header style for better visibility

--- src/components/dom/Layout/Header/index.tsx
+++ src/components/dom/Layout/Header/index.tsx
@@ -1,17 +1,35 @@
+import { useRect } from "@studio-freight/hamo";
+
 import Logo from "./Logo";
 import MenuButton from "./MenuButton";
+import { useEffect } from "react";
+import useHeaderState from "@/hooks/useHeaderState";
 
 export default function Header() {
+    const [header, headerRect] = useRect();
+    const { setHeaderHeight } = useHeaderState();
+
+    useEffect(() => {
+        if (headerRect) {
+            setHeaderHeight(headerRect.height);
+        }
+    }, [headerRect]);
     return (
-        <header className="pointer-events-auto w-full sticky top-0 left-0">
-            <ul className="relative w-full flex flex-row justify-between p-6 md:p-8">
-                <li>
-                    <Logo />
-                </li>
-                <li>
-                    <MenuButton />
-                </li>
-            </ul>
+        <header
+            className="pointer-events-auto w-full sticky top-0 left-0"
+            ref={header}
+        >
+            <div className="relative size-full">
+                <div className="absolute bg-black-pure/20 backdrop-blur-3xl top-0 left-0 size-full"></div>
+                <ul className="relative w-full flex flex-row justify-between py-4 px-6 md:px-8">
+                    <li>
+                        <Logo />
+                    </li>
+                    <li>
+                        <MenuButton />
+                    </li>
+                </ul>
+            </div>
         </header>
     );
 }
