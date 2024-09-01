9119428126f83e878c2a4b3497452c55c33bd10c
Mario Perez <mario.perez415@gmail.com>
2024-08-31 23:44:15 -0400

    Update global ts config for custom r3f components

--- global-env.d.ts
+++ global-env.d.ts
@@ -2,16 +2,16 @@ interface Window {
     [key: string]: any;
 }
 
-declare module "*.glsl" {
-    const content: string;
-    export default content;
-}
-
 declare module "*.svg" {
     import React from "react";
     export const ReactComponent: React.FunctionComponent<
         React.SVGProps<SVGSVGElement>
     >;
+}
+
+declare module "*.glsl" {
+    const content: string;
+    export default content;
 }
 
 type NumericRange<
