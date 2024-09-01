a928c82d3be4f581092d1f16bbaa4d1a427be641
Mario Perez <mario.perez415@gmail.com>
2024-09-01 02:32:02 -0400

    Create logo and menu icon animations on menu open

--- src/components/dom/Layout/Header/Logo.tsx
+++ src/components/dom/Layout/Header/Logo.tsx
@@ -2,15 +2,69 @@ import Link from "@/components/dom/common/Link";
 
 import { ReactComponent as LogoWithText } from "@/svg/logo.svg";
 import { ReactComponent as LogoMark } from "@/svg/logo-mark.svg";
-
+import { gsap, useGSAP } from "@/lib/gsap";
+import { useRef } from "react";
+import useMenuState from "@/hooks/useMenuState";
 export default function Logo() {
+    const ref = useRef<HTMLDivElement>(null);
+    const { menuOpen } = useMenuState();
+
+    useGSAP(
+        () => {
+            if (menuOpen) {
+                gsap.timeline({ paused: true })
+                    .to(".js-logo-with-text", {
+                        xPercent: -100,
+                        alpha: 0,
+                        ease: "power1.inOut",
+                    })
+                    .to(
+                        ".js-logo-mark",
+                        {
+                            xPercent: 100,
+                            alpha: 1,
+                            ease: "power1.inOut",
+                        },
+                        0
+                    )
+                    .play();
+            } else {
+                gsap.timeline({ paused: true })
+                    .to(".js-logo-with-text", {
+                        xPercent: 0,
+                        alpha: 1,
+                        ease: "power1.inOut",
+                    })
+                    .to(
+                        ".js-logo-mark",
+                        {
+                            xPercent: 0,
+                            alpha: 0,
+                            ease: "power1.inOut",
+                        },
+                        0
+                    )
+                    .play();
+            }
+        },
+        {
+            dependencies: [menuOpen],
+            scope: ref,
+        }
+    );
+
     return (
-        <Link href="/" aria-label="Home">
-            <div className="w-72">
-                <LogoWithText className="fill-white" />
-            </div>
-            <div className="w-8">
-                <LogoMark className="fill-white" />
+        <Link
+            href="/"
+            aria-label="Home"
+            className="flex p-4 flex-col items-start justify-center h-full -ml-4"
+        >
+            <div
+                className="flex h-fit flex-row items-center relative overflow-hidden"
+                ref={ref}
+            >
+                <LogoWithText className="fill-white js-logo-with-text w-64 md:w-80" />
+                <LogoMark className="js-logo-mark h-full w-auto absolute -translate-x-full opacity-0 fill-white" />
             </div>
         </Link>
     );
--- src/components/dom/Layout/Header/index.tsx
+++ src/components/dom/Layout/Header/index.tsx
@@ -4,7 +4,7 @@ import MenuButton from "./MenuButton";
 export default function Header() {
     return (
         <header className="pointer-events-auto w-full sticky top-0 left-0">
-            <ul className="relative w-full flex flex-row justify-between">
+            <ul className="relative w-full flex flex-row justify-between p-6 md:p-8">
                 <li>
                     <Logo />
                 </li>
