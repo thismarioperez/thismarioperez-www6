adf1095c0625fb364031583a970cc2555c47e484
Mario Perez <mario.perez415@gmail.com>
2024-09-01 17:50:59 -0400

    Refine content transition when menu toggles

--- src/components/dom/Layout/index.tsx
+++ src/components/dom/Layout/index.tsx
@@ -2,6 +2,8 @@
 
 import { AppProps } from "next/app";
 import dynamic from "next/dynamic";
+import cx from "classnames";
+
 import TransitionComponent from "@/components/dom/Layout/TransitionComponent";
 import Header from "@/components/dom/Layout/Header";
 import DebugHandler from "@/components/dom/Layout/DebugHandler";
@@ -11,6 +13,7 @@ import { Suspense } from "react";
 import GSAP from "@/lib/gsap/components/GSAP";
 import { ReactLenis } from "@/lib/lenis";
 import ScrollHandler from "./ScrollHandler";
+import useMenuState from "@/hooks/useMenuState";
 
 const Noise = dynamic(() => import("@/components/dom/common/Noise"), {
     ssr: false,
@@ -21,21 +24,29 @@ const Experience = dynamic(() => import("@/components/canvas/Experience"), {
 });
 
 export default function Layout({ Component, pageProps }: AppProps) {
+    const { menuOpen } = useMenuState();
     return (
         <>
             <div className="relative pointer-events-none">
                 <Noise blendMode="overlay" opacity={30} />
                 <Header />
                 <Menu />
-                <ReactLenis root>
-                    <div className="relative h-full w-full z-10">
-                        <TransitionComponent>
-                            <Component {...pageProps} />
-                        </TransitionComponent>
-                    </div>
-                    <ScrollHandler />
-                </ReactLenis>
                 <Suspense fallback={null}>
+                    <ReactLenis root>
+                        <div
+                            className={cx(
+                                "relative h-full w-full z-10 transition-all duration-1000 ease-in-out",
+                                menuOpen
+                                    ? "blur-3xl opacity-0 invisible"
+                                    : "blur-0 opacity-100 visible"
+                            )}
+                        >
+                            <TransitionComponent>
+                                <Component {...pageProps} />
+                            </TransitionComponent>
+                        </div>
+                        <ScrollHandler />
+                    </ReactLenis>
                     <Experience />
                 </Suspense>
             </div>
