a4330111c2519eeb6844bf07bd49c550a050d1c8
Mario Perez <mario.perez415@gmail.com>
2024-09-02 23:26:33 -0400

    Tweak logo scene interactions

--- src/components/canvas/LogoScene/index.tsx
+++ src/components/canvas/LogoScene/index.tsx
@@ -1,40 +1,47 @@
 import { useRef } from "react";
 import * as THREE from "three";
 import { useFrame, useThree } from "@react-three/fiber";
-import { Center, Text3D } from "@react-three/drei";
+import {
+    Center,
+    Environment,
+    Lightformer,
+    OrbitControls,
+    PerspectiveCamera,
+    Text3D,
+} from "@react-three/drei";
 
 import colors from "@/styles/colors";
-import { damp3 } from "maath/easing";
+import { damp3, dampE } from "maath/easing";
 import useDebug from "@/hooks/useDebug";
 
 const SPEED = 0.75;
 const ROTATION_X_FACTOR = 0.05;
-const ROTATION_Y_FACTOR = 0.05;
+const ROTATION_Y_FACTOR = 0.2;
 const ROTATION_Z_FACTOR = 0.05;
 
 export default function LogoScene() {
     const [debug] = useDebug();
     const mesh = useRef<THREE.Mesh>(null);
     const translationGroup = useRef<THREE.Group>(null);
-    const oscillationGroup = useRef<THREE.Group>(null);
+    const floatGroup = useRef<THREE.Group>(null);
     const { width, height } = useThree((state) => state.viewport);
-    const scale = Math.min(width, height) * 0.5;
 
     useFrame(({ clock: { elapsedTime: time }, pointer: { x, y } }, delta) => {
-        if (oscillationGroup.current) {
-            // oscillation rotation effect
-            oscillationGroup.current.rotation.x =
+        if (floatGroup.current) {
+            // float effect
+            floatGroup.current.rotation.x =
                 Math.sin(time) * SPEED * Math.PI * 0.5 * ROTATION_X_FACTOR;
-            oscillationGroup.current.rotation.y =
+            floatGroup.current.rotation.y =
                 Math.sin(time) * SPEED * Math.PI * 0.5 * ROTATION_Y_FACTOR;
-            oscillationGroup.current.rotation.z =
+            floatGroup.current.rotation.z =
                 Math.sin(time) * SPEED * Math.PI * 0.5 * ROTATION_Z_FACTOR;
         }
+
         if (translationGroup.current) {
             // mouse interaction
             damp3(
                 translationGroup.current.position,
-                [-x * 0.25, -y * 0.25, 0],
+                [-x * 0.5, -y * 0.5, 0],
                 0.5,
                 delta
             );
@@ -47,15 +54,24 @@ export default function LogoScene() {
             <pointLight position={[2, 0, 5]} intensity={20} />
             <color attach="background" args={[colors.black.DEFAULT]} />
             {debug && <axesHelper args={[10]} position={[0, 0, 0]} />}
-
             <group ref={translationGroup}>
-                <group ref={oscillationGroup}>
-                    <Center>
+                <group ref={floatGroup}>
+                    <Center
+                        onCentered={({
+                            container,
+                            height: _height,
+                            width: _width,
+                        }) =>
+                            container.scale.setScalar(
+                                Math.min(height / _height, width / _width) * 0.5
+                            )
+                        }
+                    >
                         <Text3D
                             font="/fonts/PrestigeEliteStd_Bold.json"
                             castShadow
                             receiveShadow
-                            scale={scale}
+                            // scale={scale}
                             ref={mesh}
                         >
                             M
