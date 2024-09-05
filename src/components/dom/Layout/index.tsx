49175fcce1aa21daff1af5d2100ef401c9ff3a06
Mario Perez <mario.perez415@gmail.com>
2024-09-05 01:43:30 -0400

    Remove leftovers from next-mdx-remote

--- src/components/dom/Layout/index.tsx
+++ src/components/dom/Layout/index.tsx
@@ -2,7 +2,6 @@ import { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import cx from "classnames";
 
-import MDXProvider from "@/lib/mdx/context";
 import TransitionComponent from "@/components/dom/Layout/TransitionComponent";
 import Header from "@/components/dom/Layout/Header";
 import DebugHandler from "@/components/dom/Layout/DebugHandler";
@@ -26,14 +25,10 @@ const Noise = dynamic(() => import("@/components/dom/common/Noise"), {
     ssr: false,
 });
 
-const Providers = ({ children }: { children: React.ReactNode }) => {
-    return <MDXProvider>{children}</MDXProvider>;
-};
-
 export default function Layout({ Component, pageProps }: AppProps) {
     const { menuOpen } = useMenuState();
     return (
-        <Providers>
+        <>
             <Loading />
 
             {/* canvas */}
@@ -67,6 +62,6 @@ export default function Layout({ Component, pageProps }: AppProps) {
             <DebugHandler />
             <LevaUI />
             <GSAP />
-        </Providers>
+        </>
     );
 }
