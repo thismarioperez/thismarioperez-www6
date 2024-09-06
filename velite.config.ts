307f7ed4a80ddfdfa07a92f776c5847885d3892e
Mario Perez <mario.perez415@gmail.com>
2024-09-06 15:21:24 -0400

    Update page schema to include hero schema

--- velite.config.ts
+++ velite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig, defineCollection, s } from "velite";
 
-const computedFields = <T extends { slug: string }>(data: T) => ({
-    ...data,
-    slugAsParams: data.slug.split("/").slice(1).join("/"),
+const baseHeroSchema = s.object({
+    title: s.string().max(99),
+    description: s.string().max(999).optional(),
 });
+
+const heroHomeSchema = s.object({
+    type: s.literal("home"),
+});
+
+const heroImageSchema = s.object({
+    type: s.literal("image"),
+    src: s.file({ allowNonRelativePath: true }).default("/images/image-1.jpg"),
+});
+
+const heroSchema = s
+    .discriminatedUnion("type", [heroHomeSchema, heroImageSchema])
+    .and(baseHeroSchema);
 
 const basePageSchema = s.object({
     slug: s.path(),
@@ -11,6 +24,12 @@ const basePageSchema = s.object({
     description: s.string().max(999).optional(),
     published: s.boolean().optional().default(true),
     body: s.mdx(),
+    hero: heroSchema.optional(),
+});
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+    ...data,
+    slugAsParams: data.slug.split("/").slice(1).join("/"),
 });
 
 const pages = defineCollection({
