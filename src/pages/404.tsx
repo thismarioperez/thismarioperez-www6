538d31ccd3874a05e368df2abfad5e6bfe68e837
Mario Perez <mario.perez415@gmail.com>
2024-09-07 01:32:37 -0400

    Setup 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,20 +1,12 @@
-import { Page } from "@site/content";
+import * as Themed from "@/components/dom/common/Themed";
 
-import { MDXContent } from "@/lib/content/mdx-components";
-import { getPageBySlug } from "@/lib/content";
-
-export async function getStaticProps() {
-    return {
-        props: {
-            page: getPageBySlug("404"),
-        },
-    };
-}
-
-type TPageProps = {
-    page: Page;
-};
-
-export default function Page404({ page }: TPageProps) {
-    return <MDXContent code={page.body} />;
+export default function Page404() {
+    return (
+        <div className="relative">
+            <div className="flex flex-col size-full justify-center items-center gap-y-2">
+                <Themed.H1Text as="h1">Well this is awkward...</Themed.H1Text>
+                <Themed.H2Text as="h3">Page not found</Themed.H2Text>
+            </div>
+        </div>
+    );
 }
