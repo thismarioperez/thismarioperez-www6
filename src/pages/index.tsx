437a1caeee211520c3a8a26ff147f3695742d71b
Mario Perez <mario.perez415@gmail.com>
2024-09-07 02:57:19 -0400

    Remove placeholder content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,41 +20,7 @@ type TPageProps = {
 export default function HomePage({ page }: TPageProps) {
     return (
         <div className="size-full relative">
-            <h1>Home page</h1>
             <MDXContent code={page.body} />
-            <div className="h-[100vh]"></div>
-            <Container bgColor="black" inset>
-                <Themed.H1Text as="h1">Hello world</Themed.H1Text>
-            </Container>
-            <Container bgColor="white" inset>
-                <Themed.H1Text className="text-black" as="h1">
-                    Hello world
-                </Themed.H1Text>
-            </Container>
-            <Container bgColor="yellow" inset>
-                <Themed.H1Text className="text-black" as="h1">
-                    Hello world
-                </Themed.H1Text>
-            </Container>
-            <Container bgColor="black">
-                <Themed.H1Text as="h1">Hello world</Themed.H1Text>
-            </Container>
-            <Container bgColor="white">
-                <Themed.H1Text className="text-black" as="h1">
-                    Hello world
-                </Themed.H1Text>
-            </Container>
-            <Container bgColor="yellow">
-                <Themed.H1Text className="text-black" as="h1">
-                    Hello world
-                </Themed.H1Text>
-            </Container>
-            <div className="w-full h-[50vh]"></div>
-            <div className="w-full h-[50vh] bg-black"></div>
-            <div className="w-full h-[50vh]"></div>
-            <div className="w-full h-[50vh] bg-white"></div>
-            <div className="w-full h-[50vh]"></div>
-            <div className="w-full h-[50vh] bg-black"></div>
         </div>
     );
 }
