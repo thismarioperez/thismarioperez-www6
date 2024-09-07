d6a48d18aaee60da217959789a3addb1ce0a1f6d
Mario Perez <mario.perez415@gmail.com>
2024-09-07 01:40:25 -0400

    Track header height in a css variable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,23 @@ import Layout from "@/components/dom/Layout";
 import "@/styles/globals.css";
 import Head from "next/head";
 import "@/components/canvas/extend";
+import useAppState from "@/hooks/useAppState";
 
 const sourceCodePro = Source_Code_Pro({
     subsets: ["latin"],
 });
 
 export default function App(props: AppProps) {
+    const { headerHeight } = useAppState();
     return (
         <>
             <style jsx global>{`
                 :root {
+                    /* custom font-family */
                     --source-code-pro-font: ${sourceCodePro.style.fontFamily};
+
+                    /* custom variables*/
+                    --header-height: ${headerHeight}px;
                 }
             `}</style>
             <Head>
