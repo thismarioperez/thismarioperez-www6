cc6f65ac1bc2ba85daeebe0be4792bc5268b6a35
Mario Perez <mario.perez415@gmail.com>
2024-09-07 02:44:14 -0400

    Tweak 404 page layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import * as Themed from "@/components/dom/common/Themed";
 
 export default function Page404() {
     return (
-        <div className="relative h-full">
+        <div className="relative size-full">
             <div className="flex flex-col size-full justify-center items-center gap-y-2">
                 <Themed.H1Text as="h1">Well this is awkward...</Themed.H1Text>
                 <Themed.H2Text as="h3">Page not found</Themed.H2Text>
