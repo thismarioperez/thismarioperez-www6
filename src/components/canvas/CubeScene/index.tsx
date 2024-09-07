c9a0328bc7b3498a4e37a86a169d48523690b0f9
Mario Perez <mario.perez415@gmail.com>
2024-09-07 03:43:55 -0400

    Add shadow to cube scene. Use brand yellow for texture color

--- src/components/canvas/CubeScene/index.tsx
+++ src/components/canvas/CubeScene/index.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
+import colors from "@/styles/colors";
 
 const RotatingCube = () => {
     const meshRef = useRef<THREE.Mesh>(null);
@@ -13,9 +14,11 @@ const RotatingCube = () => {
     });
 
     return (
-        <mesh ref={meshRef} position={[0, 0, 0]}>
+        <mesh ref={meshRef} position={[0, 0, 0]} castShadow receiveShadow>
+            {/* <torusGeometry args={[200, 120, 200, 200]} /> */}
+            {/* <meshToonMaterial color={colors.yellow.DEFAULT} /> */}
             <boxGeometry args={[1, 1, 1]} />
-            <meshBasicMaterial color="teal" />
+            <meshStandardMaterial color={colors.yellow.DEFAULT} />
         </mesh>
     );
 };
@@ -23,8 +26,9 @@ const RotatingCube = () => {
 export default function CubeScene() {
     return (
         <>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
+            <ambientLight intensity={1} />
+            <directionalLight position={[2, 2, 1]} intensity={1} />
+            <color attach="background" args={[colors.black.DEFAULT]} />
             <RotatingCube />
         </>
     );
