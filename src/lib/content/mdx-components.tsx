128df7f92f81f5c7bbfe74e7d5c1879a6026296c
Mario Perez <mario.perez415@gmail.com>
2024-09-06 21:03:00 -0400

    Add comment

--- src/lib/content/mdx-components.tsx
+++ src/lib/content/mdx-components.tsx
@@ -31,7 +31,7 @@ const useMDXComponent = (code: string) => {
 
 interface MDXProps {
     code: string;
-    components?: Record<string, React.ComponentType>;
+    components?: Record<string, React.ComponentType>; // Pass additional components here
 }
 
 // MDXContent component
