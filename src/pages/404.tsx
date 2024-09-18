66eac7038180670cc0177844220a64c208f54662
Mario Perez <mario.perez415@gmail.com>
2024-09-18 16:17:21 -0400

    Cleanup 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,16 @@
+import Container from "@/components/dom/common/Container";
+import Link from "@/components/dom/common/Link";
 import * as Themed from "@/components/dom/common/Themed";
 
 export default function Page404() {
     return (
         <div className="relative size-full">
-            <div className="flex flex-col size-full justify-center items-center gap-y-2">
-                <Themed.H1Text as="h1">Well this is awkward...</Themed.H1Text>
-                <Themed.H2Text as="h3">Page not found</Themed.H2Text>
+            <div className="flex flex-col w-full min-h-[calc(100vh-var(--header-height))] justify-center">
+                <Container>
+                    <h1>Well this is awkward...</h1>
+                    <h2>Looks like this page doesn't exist</h2>
+                    <Link href="/">Go drunk, you're home!</Link>
+                </Container>
             </div>
         </div>
     );
