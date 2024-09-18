7dfacf8fa1ff3a305da2692623c253fa31e31bda
Mario Perez <mario.perez415@gmail.com>
2024-09-18 01:47:19 -0400

    Setup persisted store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { create, StateCreator } from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist, createJSONStorage } from "zustand/middleware";
 
 import { createMenuSlice, IMenuSlice } from "./menuSlice";
 import { createSliderSlice, ISliderSlice } from "./sliderSlice";
@@ -13,18 +13,29 @@ export type TStoreState = IAppSlice &
 
 export type TStoreStateCreator<T> = StateCreator<
     TStoreState,
-    [["zustand/devtools", never]],
+    [["zustand/devtools", never], ["zustand/persist", any]],
     [],
     T
 >;
 
 const useStore = create<TStoreState>()(
-    devtools((...a) => ({
-        ...createAppSlice(...a),
-        ...createMenuSlice(...a),
-        ...createSliderSlice(...a),
-        ...createTransitionSlice(...a),
-    }))
+    devtools(
+        persist(
+            (...a) => ({
+                ...createAppSlice(...a),
+                ...createMenuSlice(...a),
+                ...createSliderSlice(...a),
+                ...createTransitionSlice(...a),
+            }),
+            {
+                name: "thismarioperez-store",
+                storage: createJSONStorage(() => sessionStorage),
+                partialize: (state) => ({
+                    isReady: state.isReady,
+                }),
+            }
+        )
+    )
 );
 
 export default useStore;
