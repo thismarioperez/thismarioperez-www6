057b1ef90f8932d7e8e4688d75e45dfa10fecef7
Mario Perez <mario.perez415@gmail.com>
2024-09-18 15:07:49 -0400

    Create a placeholder footer

---
+++ src/components/dom/Layout/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="p-5 relative w-full bg-yellow">
+            <div className="w-full text-black">Links go here</div>
+            <div className="w-full text-black">Links go here</div>
+            <div className="w-full text-black">Links go here</div>
+            <div className="w-full text-black">Links go here</div>
+        </footer>
+    );
+}
