90af92f4c252e6a0867a6097c88d3424aa030a62
Mario Perez <mario.perez415@gmail.com>
2024-09-18 15:33:11 -0400

    Create anchor link for scrolling to hash

---
+++ src/components/dom/common/AnchorLink.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useLenis } from "@/lib/lenis";
+import React from "react";
+
+export type TAnchorLinkProps = React.HTMLAttributes<HTMLAnchorElement>;
+const AnchorLink = ({ children, ...rest }: TAnchorLinkProps) => {
+    const lenis = useLenis();
+
+    return (
+        <a
+            {...rest}
+            onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
+                e.preventDefault();
+                if ((e.target as HTMLAnchorElement).hash) {
+                    lenis?.scrollTo((e.target as HTMLAnchorElement).hash);
+                }
+                if (rest.onClick) {
+                    rest.onClick(e);
+                }
+            }}
+        >
+            {children}
+        </a>
+    );
+};
+
+export default AnchorLink;
