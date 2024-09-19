f228462aa316f4ac0ed070268f6186c3b718e382
Mario Perez <mario.perez415@gmail.com>
2024-09-18 22:56:49 -0400

    Update default page title

--- src/components/dom/common/MetaPageTitle.tsx
+++ src/components/dom/common/MetaPageTitle.tsx
@@ -5,7 +5,7 @@ import * as constants from "@/core/constants";
 export default function MetaPageTitle({ title }: { title: string }) {
     return (
         <Head>
-            <title key="page-title">{`${constants.SITE_NAME} | ${title}`}</title>
+            <title key="page-title">{`${constants.SITE_NAME} | Creative Developer - 🤘 | ${title}`}</title>
             <meta
                 property="og:title"
                 content={`${constants.SITE_NAME} | ${title}`}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,14 @@
-import { Page } from "@site/content";
-
 import { MDXContent } from "@/lib/content/mdx-components";
-import { getPageBySlug } from "@/lib/content";
 import MetaPageTitle from "@/components/dom/common/MetaPageTitle";
+import { getPageBySlug } from "@/lib/content";
 
-export async function getStaticProps() {
-    return {
-        props: {
-            page: getPageBySlug("home"),
-        },
-    };
-}
-
-type TPageProps = {
-    page: Page;
-};
-
-export default function HomePage({ page }: TPageProps) {
+const page = getPageBySlug("home");
+export default function HomePage() {
     return (
         <>
-            <MetaPageTitle title={page.title} />
+            <MetaPageTitle title={page!.title} />
             <div className="size-full relative">
-                <MDXContent code={page.body} />
+                <MDXContent code={page!.body} />
             </div>
         </>
     );
